e6e3a3e0b11218ec290842554c88f87171f625e7
Sergey Vlasov <sigprof@gmail.com>
2020-05-17 20:22:07 +0300

    [keymap] xd87: Add the `sigprof-test` keymap
    This is a variation of my custom keymap which will be used for testing,
    therefore some debugging options are enabled in config.h.

---
+++ keyboards/xd87/keymaps/sigprof-test/rules.mk
@@ -0,0 +1,13 @@
+# Use the simplified "Bootmagic Lite" to decrease the firmware size.
+BOOTMAGIC_ENABLE = lite
+
+# Disable the Command feature, which is also rather large and not very useful
+# (most settings can be changed by using Bootmagic keycodes in the keymap).
+COMMAND_ENABLE = no
+
+# Enable mouse emulation.
+MOUSEKEY_ENABLE = yes
+
+# Enable RGB underglow (for some reason this is disabled in the default
+# configuration for this keyboard).
+RGBLIGHT_ENABLE = yes
