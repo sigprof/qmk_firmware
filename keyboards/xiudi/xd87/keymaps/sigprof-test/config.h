3da90515b38f3e35d004784d6355d2ff8d75c8e1
Sergey Vlasov <sigprof@gmail.com>
2020-05-17 20:22:07 +0300

    [keymap] xd87: Add the `sigprof-test` keymap
    This is a variation of my custom keymap which will be used for testing,
    therefore some debugging options are enabled in config.h.

---
+++ keyboards/xiudi/xd87/keymaps/sigprof-test/config.h
@@ -0,0 +1,18 @@
+// Measure the matrix scan rate and report it in the debug console.
+#define DEBUG_MATRIX_SCAN_RATE
+
+// Use the minimum possible I/O delay for the matrix to speed up scanning.
+#define MATRIX_IO_DELAY 1
+
+// Set the USB polling rate to 1000 Hz to reduce the latency to the minimum.
+#define USB_POLLING_INTERVAL_MS 1
+
+// Make the MT() behavior identical to LT() and other dual-use keys (this does
+// not really change the behavior when HOLD_ON_OTHER_KEY_PRESS is enabled, but
+// removes some extra code).
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Select the hold action for a dual-use key immediately when another key is
+// pressed (this is different from PERMISSIVE_HOLD, which selects the hold
+// action when another key is pressed and then released).
+#define HOLD_ON_OTHER_KEY_PRESS
