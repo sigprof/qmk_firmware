6bc612693ac15f78f28ff13adf346533aa58438d
Sergey Vlasov <sigprof@gmail.com>
2020-06-26 23:15:57 +0300

    [keymap] xd87:sigprof-test: Increase the number of RGB LEDs for testing

--- keyboards/xiudi/xd87/keymaps/sigprof-test/config.h
+++ keyboards/xiudi/xd87/keymaps/sigprof-test/config.h
@@ -16,3 +16,9 @@
 // pressed (this is different from PERMISSIVE_HOLD, which selects the hold
 // action when another key is pressed and then released).
 #define HOLD_ON_OTHER_KEY_PRESS
+
+// Increase the number of RGB LEDs to test the behavior with a really long
+// chain of WS2812 LEDs.  For 105 LEDs (imagine a fullsize ISO keyboard with
+// per-key RGB) sending of the bitstream would take 3.15 ms.
+#undef RGBLED_NUM
+#define RGBLED_NUM 105
