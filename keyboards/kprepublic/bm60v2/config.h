58291358bd8c047b0f243306dd34bb95c99ba049
Sergey Vlasov <sigprof@gmail.com>
2021-09-14 18:47:51 +0300

    Starting test implementation of RGB Matrix

--- keyboards/kprepublic/bm60v2/config.h
+++ keyboards/kprepublic/bm60v2/config.h
@@ -1,3 +1,10 @@
 #pragma once
 
 #include "config_common.h"
+
+#ifdef RGB_MATRIX_ENABLE
+#    define DRIVER_COUNT 1
+#    define DRIVER_ADDR_1 0b1010000
+#    define DRIVER_1_LED_TOTAL 64
+#    define DRIVER_LED_TOTAL DRIVER_1_LED_TOTAL
+#endif
