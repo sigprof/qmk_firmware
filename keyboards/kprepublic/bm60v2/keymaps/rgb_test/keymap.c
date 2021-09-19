1a1b218a3e8425b23a910fbb35d8a37e2c138198
Sergey Vlasov <sigprof@gmail.com>
2021-09-19 19:54:01 +0300

    Fix IS31FL3733 register names
    Register name constants are numbered from 1, not 0.

--- keyboards/kprepublic/bm60v2/keymaps/rgb_test/keymap.c
+++ keyboards/kprepublic/bm60v2/keymaps/rgb_test/keymap.c
@@ -105,7 +105,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                     // IS31FL3733-specific code here
                     is31_led led = g_is31_leds[rgb_led_index];
                     uint8_t  reg = (rgb_color_index == 0) ? led.r : ((rgb_color_index == 1) ? led.g : led.b);
-                    sprintf(send_buf, "%d,%c_%d", led.driver, 'A' + (reg >> 4), reg & 0x0F);
+                    sprintf(send_buf, "%d,%c_%d", led.driver, 'A' + (reg >> 4), (reg & 0x0F) + 1);
                 } else {
                     static const char colors[3] = {'R', 'G', 'B'};
                     sprintf(send_buf, "+%d,%c", rgb_led_index - ISSI_LED_TOTAL, colors[rgb_color_index]);
