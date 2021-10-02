000367583b33753abd19b99497a64441ccbb7a0e
Sergey Vlasov <sigprof@gmail.com>
2021-10-02 22:51:16 +0300

    Add `handwired/pintester/teensy_2` (PJRC Teensy 2.0)
    The configuration for Teensy 2.0 removes the E2 pin, which is not broken
    out on that board, and ignores the D6 pin by default, because that PIN
    has a LED connected to it, and this can cause problems if the pin is
    used in the keyboard matrix (depending on the LED properties, the
    voltage on the pin may be in the indeterminate range between logical low
    and high levels, so the behavior might be random).

---
+++ keyboards/handwired/pintester/teensy_2/config.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2021 Sergey Vlasov <sigprof@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+#define PRODUCT Pin Tester Teensy 2.0
+
+/*
+ * List of all GPIO pins available for the keyboard matrix.  Each element of
+ * the list must be one of the following macros:
+ *   - PT_PIN(pin) - normal pin;
+ *   - PT_PIN_LABEL(pin, label) - pin for which the label on the actual PCB is
+ *     significantly different from the QMK pin name.
+ */
+#define PINTESTER_PINS  \
+        PT_PIN(B0),     \
+        PT_PIN(B1),     \
+        PT_PIN(B2),     \
+        PT_PIN(B3),     \
+        PT_PIN(B4),     \
+        PT_PIN(B5),     \
+        PT_PIN(B6),     \
+        PT_PIN(B7),     \
+        PT_PIN(C6),     \
+        PT_PIN(C7),     \
+        PT_PIN(D0),     \
+        PT_PIN(D1),     \
+        PT_PIN(D2),     \
+        PT_PIN(D3),     \
+        PT_PIN(D4),     \
+        PT_PIN(D5),     \
+        PT_PIN(D6),     \
+        PT_PIN(D7),     \
+        PT_PIN(E6),     \
+        PT_PIN(F0),     \
+        PT_PIN(F1),     \
+        PT_PIN(F4),     \
+        PT_PIN(F5),     \
+        PT_PIN(F6),     \
+        PT_PIN(F7)
+
+// The D6 pin has a LED on it, which results in an undefined logical level if
+// that pin is used as an input.
+#define PINTESTER_DEFAULT_IGNORE_PINS   D6
+
+/*
+ * Total number of GPIO pins available for the keyboard matrix (must match the
+ * number of items in PINTESTER_PINS).
+ */
+#define PINTESTER_PIN_COUNT 25
