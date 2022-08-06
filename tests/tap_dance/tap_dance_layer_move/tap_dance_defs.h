3ff61d0694829b150981a7ce8f5a34287ed6ad24
Sergey Vlasov <sigprof@gmail.com>
2022-08-06 13:25:18 +0300

    Add tests for ACTION_TAP_DANCE_LAYER_MOVE()

---
+++ tests/tap_dance/tap_dance_layer_move/tap_dance_defs.h
@@ -0,0 +1,21 @@
+// Copyright 2022 Sergey Vlasov (@sigprof)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum custom_keycodes {
+    CUST_XY = SAFE_RANGE,
+    CUST_ZW,
+};
+
+enum tap_dance_ids {
+    TD_L_MOVE,
+};
+
+#ifdef __cplusplus
+}
+#endif
