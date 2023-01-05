8d41f588be674eff452458440acd3b992b27f3bd
Sergey Vlasov <sigprof@gmail.com>
2023-01-05 21:23:35 +0300

    pintester: Move matrix size from info.json to config.h
    Setting the matrix size from `info.json` does not work if a custom
    matrix is used; use `config.h` instead (this is not a major loss,
    because the pintester-specific options require `config.h` anyway, and it
    is possible to add comments to `config.h` options without thinking about
    which dialect of not-quite-JSON we are using today).

--- keyboards/handwired/pintester/config.h
+++ keyboards/handwired/pintester/config.h
@@ -19,6 +19,11 @@
 
 #include "config_common.h"
 
+// Always use 32 logical columns to support the maximum possible number of pins
+// (89*89 bits would require 248 rows; 90*90 bits would require 254 rows, which
+// won't work due to overlap with some special values).
+#define MATRIX_COLS 32
+
 /* Disable features which cannot be used by this “keyboard”. */
 #define NO_ACTION_TAPPING
 #define NO_ACTION_LAYER
